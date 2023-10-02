17698233d1b320e3e2cd2ed6bf2b9b505e96bbfb
Parth-dotcom <parthkurani41@gmail.com>
2023-10-02 19:05:54 +0530

    List function Added

--- app.py
+++ app.py
@@ -1,24 +1,32 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.db'
 db = SQLAlchemy(app)
+
+
 # Define the Student model
 class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     age = db.Column(db.Integer, nullable=False)
+
+
 # Routes
 @app.route('/')
 def index():
     db.create_all()
     students = Student.query.all()
     return render_template('index.html', students=students)
+
+
 @app.route('/show')
 def show():
     db.create_all()
     students = Student.query.all()
     return render_template('show.html', students=students)
+
 
 @app.route('/add', methods=['GET', 'POST'])
 def add_student():
@@ -30,6 +38,8 @@ def add_student():
         db.session.commit()
         return redirect(url_for('show'))
     return render_template('add.html')
+
+
 @app.route('/edit/<int:id>', methods=['GET', 'POST'])
 def edit_student(id):
     student = Student.query.get(id)
@@ -39,11 +49,20 @@ def edit_student(id):
         db.session.commit()
         return redirect(url_for('show'))
     return render_template('edit.html', student=student)
+
+
 @app.route('/delete/<int:id>')
 def delete_student(id):
     student = Student.query.get(id)
     db.session.delete(student)
     db.session.commit()
-    return redirect(url_for('index'))
+    return redirect(url_for('show'))
+
+
+@app.route('/list')
+def list():
+    return redirect(url_for('show'))
+
+
 if __name__ == '__main__':
     app.run(debug=True)
